f67b018a96f3467a8eaf924ff8bfa432c71b0bbc
ByteNadeem <nadeemzachav@hotmail.com>
2025-07-23 19:00:50 +0100

    env.py secret key hidden

--- ticksy/settings.py
+++ ticksy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7_5x(s=y)^w6eyekb0vcohy@quh31mg6$6z_=-r6q0u$2!^$yg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
