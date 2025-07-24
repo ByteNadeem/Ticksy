c544486fea6e2c6d0e01918fccf2f33ebba2e78f
ByteNadeem <nadeemzachav@hotmail.com>
2025-07-24 15:19:10 +0100

    setting database in settings.py

--- ticksy/settings.py
+++ ticksy/settings.py
@@ -12,12 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Import environment variables
-try:
-    import env
-except ImportError:
-    pass
+#try:
+#    import env
+#except ImportError:
+#    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
@@ -89,6 +93,10 @@ WSGI_APPLICATION = 'ticksy.wsgi.application'
 #       'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
