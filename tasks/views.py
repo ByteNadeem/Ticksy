295b1a948560800c9bf895c3d1d2b6de28457e16
ByteNadeem <nadeemzachav@hotmail.com>
2025-07-28 10:41:44 +0100

    Update views.py: Add form handling and separate to_do/done tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Task, Category
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 
 def home(request):
-    tasks = Task.objects.all().order_by('-due_date')
-    categories = Category.objects.all()
+    """
+    A view to display the tasks to do and the completed tasks
+    with the tasks due soonest at the top
+    """
+
+    to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('-due_date')
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
 
     context = {
-        'tasks': tasks,
-        'categories': categories,
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+        'form': form,
     }
+
     return render(request, 'tasks.html', context)
