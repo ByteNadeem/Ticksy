fb00f60ca824f7079f3959edac1dd56fd967c93a
ByteNadeem <nadeemzachav@hotmail.com>
2025-07-29 12:38:02 +0100

    login and logout HTML replaced, created base html

--- ticksy/settings.py
+++ ticksy/settings.py
@@ -58,8 +58,14 @@ INSTALLED_APPS = [
     
 ]
 SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -81,7 +87,7 @@ ROOT_URLCONF = 'ticksy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
